afed653406af625e0c7ded2c065376dd7f86d717
Johan <joh.pik.fr@gmail.com>
2018-11-13 06:37:09 +1000

    Create all initials Component

---
+++ src/MapHeader.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+class MapHeader extends Component {
+
+    render(){
+        return(
+              <div>holla</div>
+        )
+    }
+  }
+
+export default MapHeader
