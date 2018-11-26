420b69ae66053785dcd2c950423b84ba661b0df6
Johan <joh.pik.fr@gmail.com>
2018-11-27 06:04:12 +1000

    Home inpput Field open when city button is clicked

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
 class Home extends Component {
+  state = {
+    citySearch : false
+  }
+
+  ActivateCitySearch = () => {
+    this.setState({ citySearch : true })
+    console.log(this.state);
+  }
     checkCafes = () => {
       if (this.props.allCafes.length < 1) {
         let url = "/"
@@ -13,16 +21,33 @@ class Home extends Component {
     }
 
     render(){
+
       return (
         <div className="home-main">
-            <h2 className="home-heading">Where are you?</h2>
-            <button onClick={this.props.userLocation}>Near You</button>
-            <input className="search-café" placeholder="Enter your location"/>
-            <Link to={this.checkCafes()} className="search-input"><i className="fas fa-search"></i></Link>
-            <p className="cafes-numbers">We found <span>{this.props.activeMarkers.length}</span> cafés</p>
-        </div>
 
-      )
+        { !this.state.citySearch ? (
+          <Fragment>
+            <h2 className="home-heading">Search Cafés from your</h2>
+            <div className="home-buttons">
+              <button onClick={this.props.userLocation} className="home-buttons">Near You</button>
+              <span>or</span>
+              <button onClick={this.ActivateCitySearch} className="home-buttons">City</button>
+            </div>
+          </Fragment>
+        )
+
+          : (
+            <Fragment>
+              <h2 className="home-heading">Search Cafés from your</h2>
+              <input className="search-café" placeholder="Enter your location"/>
+              <Link to={this.checkCafes()} className="search-input"><i className="fas fa-search"></i></Link>
+              <p className="cafes-numbers">We found <span>{this.props.activeMarkers.length}</span> cafés</p>
+            </Fragment>
+          )
+        }
+
+        </div>
+        )
     }
 }
 
