c2f68ac6f92917f37ad33885a91be3cc8533e05f
Johan <joh.pik.fr@gmail.com>
2018-12-17 21:35:38 +1000

    Removed extra messages when user location loads

--- src/HomeResults.js
+++ src/HomeResults.js
@@ -13,15 +13,15 @@ class HomeResults extends Component {
                   this.props.activeMarkers.length > 0 && this.props.curentLocation ?
                     (<Fragment>
                       <p className="home-cafes-numbers">We found <span>{this.props.activeMarkers.length}</span> cafés <span>{this.props.mapCenter.name}</span></p>
-                      <button className="home-button home-button-result">
+                      <button className="home-button map-result">
                         <Link to="/map">
                           See Results on Map
                         </Link>
                       </button>
                       <span>or</span>
-                      <button onClick={() => this.props.resetApp()}><i className="fas fa-arrow-circle-left"/>Return to Menu</button>
+                      <button className="home-button back-menu" onClick={() => this.props.resetApp()}><i className="fas fa-arrow-circle-left"/>Return to Menu</button>
                     </Fragment>)
-                  :
+                  : this.props.curentLocation ? (<Fragment/>) :
                     (<p>Please click one of the options above</p>)
 
               /**** Using Input Search City ***/
