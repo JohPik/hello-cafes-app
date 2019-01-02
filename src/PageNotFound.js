749529d36e61504ea7512062be4b23baaa40f88b
Johan <joh.pik.fr@gmail.com>
2019-01-02 10:34:59 +1000

    Styled Page Not Found

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -1,16 +1,16 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component} from 'react'
 import { Link } from 'react-router-dom'
 
 class PageNotFound extends Component {
 
     render(){
       return (
-        <Fragment>
-            <h1>Did you just drop your coffee cup?</h1>
-            <h2>Error 404</h2>
-            <h3>the page you are looking for does not exist</h3>
-            <Link  to="/" className="home"><i className="fas fa-search"></i>return to Search Page</Link>
-        </Fragment>
+        <div className="page-not-found">
+            <h1>Error 404</h1>
+            <h2>Did you just drop your coffee cup?</h2>
+            <p>the page you are looking for does not exist</p>
+            <Link  to="/" className="home"><button className="home-button back-menu"><i className="fas fa-arrow-circle-left"/>Return to Search Page</button></Link>
+        </div>
 
       )
     }
